d00d707035f6d2bd5cd7da7342b83c76791eb2eb
Dironmarek <77089372+Dironmarek@users.noreply.github.com>
2022-12-02 22:23:08 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+import functions
+import tkinter as tk
+turn = 0
+counter = 0
+board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+flag_thing = True
+root=tk.Tk()
+
+root.geometry("300x300")
+
+canvas=tk.Canvas(root, width=999, height=999)
+canvas.pack()
+
+def click(event):
+  global turn
+  global counter
+  global menu_button
+  global board
+  global flag_thing
+  flag = False
+  for i in range(3):
+    for j in range(3):
+      if j == functions.get_cellpos(event)[0] and i == functions.get_cellpos(event)[1] and board[j][i]==0 and flag_thing:
+        if turn == 0:
+          canvas.create_line(100*i,100*j,100*(i+1),100*(j+1), fill="red", width=3)
+          canvas.create_line(100*(i+1),100*j,100*i,100*(j+1), fill="red", width=3)
+          board[j][i] = 1 #x
+        else:
+          canvas.create_oval(100*(i+1),100*j,100*i,100*(j+1), outline="blue",width=3)
+          board[j][i] = 2 #o
+        flag = True
+        break
+  if functions.detect_win(board) == 1:
+    turn = 0
+    counter = 0
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    flag_thing = False
+    canvas.delete('all')
+    canvas.create_text(150, 125, text="X wins", fill="black", font=('Helvetica 15 bold'))
+    menu_button=tk.Button(root, text="Menu", command = menu)
+    menu_button.place(x=120, y=150)
+  elif functions.detect_win(board) == 2:
+    turn = 0
+    counter = 0
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    flag_thing = False
+    canvas.delete('all')
+    canvas.create_text(150, 125, text="O wins", fill="black", font=('Helvetica 15 bold'))
+    menu_button=tk.Button(root, text="Menu", command = menu)
+    menu_button.place(x=125, y=150)
+  if counter == 8:
+    turn = 0
+    counter = 0
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    flag_thing = False
+    canvas.delete('all')
+    canvas.create_text(150, 120, text="Draw", fill="black", font=('Helvetica 15 bold'))
+    menu_button=tk.Button(root, text="Menu", command = menu)
+    menu_button.place(x=122, y=150)
+    counter=0
+  if flag:
+    turn += 1
+    turn %= 2
+    counter += 1
+
+
+def main():
+  global flag_thing
+  flag_thing = True
+  canvas.delete('all')
+  play.place_forget()
+  canvas.create_line(100,0,100,300, fill="black", width=3)  
+  canvas.create_line(200,0,200,300, fill="black", width=3)
+  canvas.create_line(0,100,300,100, fill="black", width=3)
+  canvas.create_line(0,200,300,200, fill="black", width=3)
+  canvas.bind("<Button>", click)
+
+
+def menu():
+  global play
+  canvas.delete('all')
+  menu_button.place_forget()
+  canvas.create_text(150,70, text="Menu", fill="black",font=('Helvetica 15 bold'))
+  
+  play=tk.Button(root, text="play", command = main)
+  play.place(x=125, y=90)
+
+canvas.delete('all')
+canvas.create_text(150,70, text="Menu", fill="black",font=('Helvetica 15 bold'))
+play=tk.Button(root, text="play", command = main)
+play.place(x=125, y=90)
+
+root.mainloop()
+
